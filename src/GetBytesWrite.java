254961edff0869af84c101debc0c52ecfccb7a21
Ilya Maximov <xc.navi@gmail.com>
2022-02-22 15:06:10 +0200

    Added getBytes function

--- src/GetBytesWrite.java
+++ src/GetBytesWrite.java
@@ -1,11 +1,26 @@
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class GetBytesWrite {
     public static void main(String[] args) throws IOException {
-        FileOutputStream fos = new FileOutputStream("files/write.txt");
-        fos.write("Превед! Медвед!".getBytes("UTF-8"));
+
+        String path = "files/write.txt";
+        File f = new File(path);
+        if (f.exists() == true) {
+            System.out.println("File exists");
+        } else System.out.println("File not exist");
+
+        FileOutputStream fos = new FileOutputStream(path);
+        fos.write("Превед!".getBytes("UTF-8"));
         fos.close();
+        if (f.exists() == true) {
+            System.out.println("File created");
+            f.delete();
+            System.out.println("File deleted");
+        } else System.out.println("not created");
+
+        System.out.println("Encoding: " + System.getProperty("file.encoding"));
 
     }
 }
